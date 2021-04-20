3dfd8ee8a96c1f2ade13d1732419610911652030
ykawa727 <y.aikawa727@gmail.com>
2021-04-20 10:11:02 +0000

    [Start] 4-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,8 +8,11 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if@post_image.save
+      redirect_to post_images_path
+    else
+      redirect_to new_post_image_path
+    end
   end
 
   def index
