8c16d9593de435c917a01c6587aa2619e5f634bd
ykawa727 <y.aikawa727@gmail.com>
2021-04-20 06:02:23 +0000

    [Finish] 4-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,9 @@ class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image #ここを追加（_idは含めない）
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
-
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
